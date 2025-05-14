085c36f97fc934d4867de95b93c8d33e2290511d
Jimb0Shmimb0 <jshmit325@gmail.com>
2025-05-15 01:06:31 +1000

    Tweaked constants

--- constants_and_enums/constants.py
+++ constants_and_enums/constants.py
@@ -2,7 +2,7 @@ import math
 
 WALL_SOUND_ABSORPTION_COEFF = 0.5
 PANEL_SOUND_ABSORPTION_COEFF = 0.7
-SOURCE_SOUND = 60 # In decibels, From 1 meter away from the source
+SOURCE_SOUND = 50 # In decibels, From 1 meter away from the source
 HEARING_THRESHOLD = 25 # In decibels
 
 
@@ -10,7 +10,7 @@ HEARING_THRESHOLD = 25 # In decibels
 SOURCE_CIRCLE_RADIUS = 0.25
 VECTOR_SIZE = 0.003
 INF = math.inf
-NO_OF_SEGMENTS = 50
+NO_OF_SEGMENTS = 75
 
 WINDOW_MARGIN = 1
-TICK = 0.0005
+TICK = 0.00001
